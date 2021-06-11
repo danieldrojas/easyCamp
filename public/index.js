671a75a982c9bf62db712fb6bda461032d898726
danieldrojas <daniel.drojas@outlook.com>
2021-06-10 22:57:41 -0500

    change form ids and get input fields refs from form

--- public/index.js
+++ public/index.js
@@ -13,3 +13,16 @@ const script = document.createElement("script");
 script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAidpXcu5bIMLxK1NGJr5MyCtN2tThm2as&libraries=geometry,places,localContext&callback=initMap`;
 script.async = true;
 document.head.appendChild(script);
+
+//variables and constants
+let trip = {};
+const originField = document.getElementById("origin");
+const destinationField = document.getElementById("destination");
+const formSubmitBtn = document.getElementById("formSubmitBtn");
+const milesPerDayField = document.getElementById("milesPerDay");
+let origin;
+let destination;
+let distanceToStop;
+console.log(formSubmitBtn);
+
+console.log();
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const path = require("path");
-// const fs = require("fs");
+const cors = require("cors");
 
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
+  console.log(req);
+  //   res.end("Server");
   //   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
