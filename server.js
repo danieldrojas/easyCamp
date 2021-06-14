ec4c3551cc3b178afee1ba9d1c99a3929eab3b2c
danieldrojas <daniel.drojas@outlook.com>
2021-06-14 14:12:13 -0500

    creat initial views

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require("express");
-const path = require("path");
 const cors = require("cors");
-const router = express.Router();
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
 app.use(cors());
 app.set("view engine", "ejs");
 
@@ -15,13 +11,6 @@ const indexRoutes = require("./routes/index");
 
 //Routes:
 app.use("/", indexRoutes);
-app.use(express.static(path.join(__dirname, "/public")));
-
-// app.post("/google-api/map", (req, res) => {
-//   console.log(req.body);
-//   res.send("Server");
-//   //   res.sendFile(path.join(__dirname, "/public/index.html"));
-// });
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
